5e6c4ade657af0f61f89a315f7aa4f6582e04193
Ajadi Paul <ajadi.ololade@gmail.com>
2019-12-13 12:23:53 +0100

    medic_admin

---
+++ accounts/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+def signup(request):
+    return render(request, 'signup.html')
+
+
+def signup_2(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
